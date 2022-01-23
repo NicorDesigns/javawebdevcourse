11a175290708c390115decd417cd78dd4f232415
NicorDesigns <nicocpk@gmail.com>
2022-01-23 09:19:36 -0500

    Got the Hello World Servlet Greeting to work

--- jee8webarchetype/src/main/java/com/nicordesigns/HelloWorldServlet.java
+++ jee8webarchetype/src/main/java/com/nicordesigns/HelloWorldServlet.java
@@ -6,29 +6,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class HelloWorldServlet extends HttpServlet
-{
-    /**
+public class HelloWorldServlet extends HttpServlet {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
-        response.getWriter().println("Hello, World!");
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().println("Hello, World!");
+	}
 
-    @Override
-    public void init() throws ServletException
-    {
-        System.out.println("Servlet " + this.getServletName() + " has started.");
-    }
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started.");
+	}
 
-    @Override
-    public void destroy()
-    {
-        System.out.println("Servlet " + this.getServletName() + " has stopped.");
-    }
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped.");
+	}
 }
--- jee8webarchetype/src/main/webapp/index.jsp
+++ jee8webarchetype/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
+<head>
+    <title>Hello, World Application Index File</title>
+  </head>
 <body>
 <h2>Hello World!</h2>
+<a href="greeting">Go to greeting</a>.
 </body>
 </html>
