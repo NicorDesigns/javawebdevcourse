e5c704cea22e647cd2c10900c058ab6e1eebe503
NicorDesigns <nicocpk@gmail.com>
2022-02-08 08:57:45 -0500

    Update working example

---
+++ charity-registration/src/main/java/com/nicordesigns/RegistrationServlet.java
@@ -0,0 +1,297 @@
+package com.nicordesigns;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@WebServlet(
+        name = "registrationServlet",
+        urlPatterns = {"/registrations"},
+        loadOnStartup = 1
+)
+@MultipartConfig(
+        fileSizeThreshold = 5_242_880, //5MB
+        maxFileSize = 20_971_520L, //20MB
+        maxRequestSize = 41_943_040L //40MB
+)
+public class RegistrationServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 4441314868375700166L;
+
+	private volatile int REGISTRATION_ID_SEQUENCE = 1;
+
+    private Map<Integer, Registration> registrationDatabase = new LinkedHashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String action = request.getParameter("action");
+        if(action == null)
+            action = "list";
+        switch(action)
+        {
+            case "create":
+                this.showRegistrationForm(response);
+                break;
+            case "view":
+                this.viewRegistration(request, response);
+                break;
+            case "download":
+                this.downloadAttachment(request, response);
+                break;
+            case "list":
+            default:
+                this.listRegistrations(response);
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String action = request.getParameter("action");
+        if(action == null)
+            action = "list";
+        switch(action)
+        {
+            case "create":
+                this.createRegistration(request, response);
+                break;
+            case "list":
+            default:
+                response.sendRedirect("registrations");
+                break;
+        }
+    }
+
+    private void showRegistrationForm(HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PrintWriter writer = this.writeHeader(response);
+
+        writer.append("<h2>Create a Registration</h2>\r\n");
+        writer.append("<form method=\"POST\" action=\"registrations\" ")
+              .append("enctype=\"multipart/form-data\">\r\n");
+        writer.append("<input type=\"hidden\" name=\"action\" ")
+              .append("value=\"create\"/>\r\n");
+        writer.append("Your Name<br/>\r\n");
+        writer.append("<input type=\"text\" name=\"customerName\"/><br/><br/>\r\n");
+        writer.append("Subject<br/>\r\n");
+        writer.append("<input type=\"text\" name=\"subject\"/><br/><br/>\r\n");
+        writer.append("Body<br/>\r\n");
+        writer.append("<textarea name=\"body\" rows=\"5\" cols=\"30\">")
+              .append("</textarea><br/><br/>\r\n");
+        writer.append("<b>Attachments</b><br/>\r\n");
+        writer.append("<input type=\"file\" name=\"file1\"/><br/><br/>\r\n");
+        writer.append("<input type=\"submit\" value=\"Submit\"/>\r\n");
+        writer.append("</form>\r\n");
+
+        this.writeFooter(writer);
+    }
+
+    private void viewRegistration(HttpServletRequest request,
+                            HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String idString = request.getParameter("registrationId");
+        Registration registration = this.getRegistration(idString, response);
+        if(registration == null)
+            return;
+
+        PrintWriter writer = this.writeHeader(response);
+
+        writer.append("<h2>Registration #").append(idString)
+              .append(": ").append(registration.getSubject()).append("</h2>\r\n");
+        writer.append("<i>Customer Name - ").append(registration.getCustomerName())
+              .append("</i><br/><br/>\r\n");
+        writer.append(registration.getBody()).append("<br/><br/>\r\n");
+
+        if(registration.getNumberOfAttachments() > 0)
+        {
+            writer.append("Attachments: ");
+            int i = 0;
+            for(Attachment attachment : registration.getAttachments())
+            {
+                if(i++ > 0)
+                    writer.append(", ");
+                writer.append("<a href=\"registrations?action=download&registrationId=")
+                      .append(idString).append("&attachment=")
+                      .append(attachment.getName()).append("\">")
+                      .append(attachment.getName()).append("</a>");
+            }
+            writer.append("<br/><br/>\r\n");
+        }
+
+        writer.append("<a href=\"registrations\">Return to list registrations</a>\r\n");
+
+        this.writeFooter(writer);
+    }
+
+    private void downloadAttachment(HttpServletRequest request,
+                                    HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String idString = request.getParameter("registrationId");
+        Registration registration = this.getRegistration(idString, response);
+        if(registration == null)
+            return;
+
+        String name = request.getParameter("attachment");
+        if(name == null)
+        {
+            response.sendRedirect("registrations?action=view&registrationId=" + idString);
+            return;
+        }
+
+        Attachment attachment = registration.getAttachment(name);
+        if(attachment == null)
+        {
+            response.sendRedirect("registrations?action=view&registrationId=" + idString);
+            return;
+        }
+
+        response.setHeader("Content-Disposition",
+                "attachment; filename=" + attachment.getName());
+        response.setContentType("application/octet-stream");
+
+        ServletOutputStream stream = response.getOutputStream();
+        stream.write(attachment.getContents());
+    }
+
+    private void listRegistrations(HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PrintWriter writer = this.writeHeader(response);
+
+        writer.append("<h2>registrations</h2>\r\n");
+        writer.append("<a href=\"registrations?action=create\">Create Registration")
+              .append("</a><br/><br/>\r\n");
+
+        if(this.registrationDatabase.size() == 0)
+            writer.append("<i>There are no registrations in the system.</i>\r\n");
+        else
+        {
+            for(int id : this.registrationDatabase.keySet())
+            {
+                String idString = Integer.toString(id);
+                Registration registration = this.registrationDatabase.get(id);
+                writer.append("Registration #").append(idString)
+                      .append(": <a href=\"registrations?action=view&registrationId=")
+                      .append(idString).append("\">").append(registration.getSubject())
+                      .append("</a> (customer: ").append(registration.getCustomerName())
+                      .append(")<br/>\r\n");
+            }
+        }
+
+        this.writeFooter(writer);
+    }
+
+    private void createRegistration(HttpServletRequest request,
+                              HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        Registration registration = new Registration();
+        registration.setCustomerName(request.getParameter("customerName"));
+        registration.setSubject(request.getParameter("subject"));
+        registration.setBody(request.getParameter("body"));
+
+        Part filePart = request.getPart("file1");
+        if(filePart != null && filePart.getSize() > 0)
+        {
+            Attachment attachment = this.processAttachment(filePart);
+            if(attachment != null)
+                registration.addAttachment(attachment);
+        }
+
+        int id;
+        synchronized(this)
+        {
+            id = this.REGISTRATION_ID_SEQUENCE++;
+            this.registrationDatabase.put(id, registration);
+        }
+
+        response.sendRedirect("registrations?action=view&registrationId=" + id);
+    }
+
+    private Attachment processAttachment(Part filePart)
+            throws IOException
+    {
+        InputStream inputStream = filePart.getInputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        int read;
+        final byte[] bytes = new byte[1024];
+
+        while((read = inputStream.read(bytes)) != -1)
+        {
+            outputStream.write(bytes, 0, read);
+        }
+
+        Attachment attachment = new Attachment();
+        attachment.setName(filePart.getSubmittedFileName());
+        attachment.setContents(outputStream.toByteArray());
+
+        return attachment;
+    }
+
+    private Registration getRegistration(String idString, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        if(idString == null || idString.length() == 0)
+        {
+            response.sendRedirect("registrations");
+            return null;
+        }
+
+        try
+        {
+            Registration registration = this.registrationDatabase.get(Integer.parseInt(idString));
+            if(registration == null)
+            {
+                response.sendRedirect("registrations");
+                return null;
+            }
+            return registration;
+        }
+        catch(Exception e)
+        {
+            response.sendRedirect("registrations");
+            return null;
+        }
+    }
+
+    private PrintWriter writeHeader(HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.append("<!DOCTYPE html>\r\n")
+              .append("<html>\r\n")
+              .append("    <head>\r\n")
+              .append("        <title>Customer Support</title>\r\n")
+              .append("    </head>\r\n")
+              .append("    <body>\r\n");
+
+        return writer;
+    }
+
+    private void writeFooter(PrintWriter writer)
+    {
+        writer.append("    </body>\r\n").append("</html>\r\n");
+    }
+}
---
+++ docs/jee8fileupload.md
@@ -0,0 +1,34 @@
+## Using a servlet to upload files to a new JEE 8 Web App Module
+
+##### [JEE 8 File Upload Start Branch](https://github.com/NicorDesigns/javawebdevcourse/tree/jee8web-file-upload-start)
+
+### 1. Presenting the Charity Tax Document Registration project
+
+#### Our Example Use Case
+
+#### We run an online database of government registered Charities and Non-Profits in South Africa
+
+#### Our example project will allow charities to "register" with us and send is their official tax registration forms 
+
+##### Charities should be able to register their information online and provide attached files such as non-profit tax registration status and address details
+##### We will also provide a "ChatBot" feature to guide potential charities who want to register with us
+##### South Africa has 11 official languages and our example project will have to be able to support all of them
+##### Because we handle sensitive information our example project will have to be really secure
+ 
+
+
+### 2. Our example project starting layout
+
+###### One Servlet - Three pages  
+
+	1. A list of registrations
+	2. A page to register - POST down-load-able Tax Registration Files
+	3. A page to view a registration
+
+    
+
+Check in the end git branch of this slide show 
+##### [JEE 8 File Upload Finish Branch](https://github.com/NicorDesigns/javawebdevcourse/tree/jee8web-file-upload-finish)
+
+    
+
